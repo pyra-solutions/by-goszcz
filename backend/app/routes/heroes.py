0e53c36deb1e0ef19132aec9c3fac195c65e01c1
Maximilian Gaedig <mg@maximiliangaedig.com>
2025-12-03 02:02:11 +0100

    deprecated functions

--- backend/app/routes/heroes.py
+++ backend/app/routes/heroes.py
@@ -10,7 +10,7 @@ router = APIRouter()
 
 @router.post("/heroes/", response_model=HeroRead)
 def create_hero(*, session: Session = Depends(get_db), hero: HeroCreate):
-    db_hero = Hero.from_orm(hero)
+    db_hero = Hero.model_validate(hero)
     session.add(db_hero)
     session.commit()
     session.refresh(db_hero)
@@ -41,7 +41,7 @@ def update_hero(*, session: Session = Depends(get_db), hero_id: int, hero: HeroU
     db_hero = session.get(Hero, hero_id)
     if not db_hero:
         raise HTTPException(status_code=404, detail="Hero not found")
-    hero_data = hero.dict(exclude_unset=True)
+    hero_data = hero.model_dump(exclude_unset=True)
     for key, value in hero_data.items():
         setattr(db_hero, key, value)
     session.add(db_hero)
