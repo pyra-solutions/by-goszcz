a9aa9125c5c14581a312fbe7047feabedb1af7ca
Witut2005 <wiktor.glogowski@onet.pl>
2025-12-06 17:20:56 +0100

    title filter

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Route, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { ProjectService } from './services/project.service';
 
 @Component({
--- frontend/src/app/page/main/main.page.ts
+++ frontend/src/app/page/main/main.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal, Signal } from '@angular/core';
 import { Router } from '@angular/router';
 import { Project } from '../../models/project.model';
 import { ProjectService } from '../../services/project.service';
@@ -11,13 +11,25 @@ import { ProjectService } from '../../services/project.service';
 })
 export class MainPage {
   selected!: Project;
-  projects: Project[] = []
+
+  titleFilter = signal('')
+
+  projects = signal<Project[]>([]);
+
+  filteredProjects = computed(()=>
+    this.projects().filter((p)=>p.title!.includes(this.titleFilter()))
+  )
 
   constructor(private router: Router, private projectService: ProjectService) {
-    this.projects = this.projectService.generateProjects(50).map((p)=>({...p, selected: false}))
+    this.projects.set(this.projectService.generateProjects(50).map((p)=>({...p, selected: false})))
+
+    setInterval(()=>{
+      console.log(this.titleFilter())
+    }, 2500)
   }
 
   goToDetails() {
     this.router.navigate(['details', this.selected.year, this.selected.pos]);
   }
+
 }
