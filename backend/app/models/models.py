55e26109de58017f08769c96b0e56298fd248e07
mutownik <ignacy.sydow@gmail.com>
2025-12-06 20:13:14 +0100

    fix

--- backend/app/models/models.py
+++ backend/app/models/models.py
@@ -76,22 +76,5 @@ class Response(SQLModel, table=True):
     pdf_url: str
     analysis: str
 
-class Consults(SQLModel, table=True):
-    __tablename__ = "consults"
-
-    project_id: int = Field(default=None, primary_key=True, sa_column("project_id", Integer, unique=True))
-    project_url: str
-    form_url: str
-    report_url: str
-
-class Comments(SQLModel, table=True):
-    __tablename__ = "comments"
-
-
-    id: int = Field(sa_column_kwargs={"name": "id_serial"}, primary_key=True)
-    consults_id: int = Field(default=None, foreign_key="consults.project_id")
-    person_name: str
-    question_number: str
-    comment: str
 
 
