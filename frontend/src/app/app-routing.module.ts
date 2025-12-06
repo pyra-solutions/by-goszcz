4bf3120739cfe6a34b6c6f1f7c0e3644fcd78b1c
Witut2005 <wiktor.glogowski@onet.pl>
2025-12-06 16:40:09 +0100

    consultations module

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { MainPage } from './page/main/main.page';
 import { DetailsPage } from './page/details/details.page';
+import { ConsultationsPage } from './page/consultations/consultations.page';
 
 const routes: Routes = [
   {path: 'home', component: MainPage},
+  {path: 'consultations', component: ConsultationsPage},
   {path: 'details/:year/:pos', component: DetailsPage},
   {path: '', redirectTo: 'home', pathMatch: 'full'}
 ];
