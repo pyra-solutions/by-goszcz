b6781175a292bfa50fb46fa79fe9a97b9680c481
mutownik <ignacy.sydow@gmail.com>
2025-12-07 06:53:40 +0100

    feat: added pdf link to main page

--- frontend/src/app/page/main/main.page.ts
+++ frontend/src/app/page/main/main.page.ts
@@ -94,4 +94,8 @@ export class MainPage {
       this.generatingSummary = false;
     })
   }
+
+  goToPDF(adress: string, eli: string){
+    return `https://isap.sejm.gov.pl/isap.nsf/download.xsp/${eli}/O/${adress.replaceAll('/','-')}.pdf` 
+  }
 }
